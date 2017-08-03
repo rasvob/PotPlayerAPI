26324beb9b661369c158bff71c710f3db24ef8c9
Radek Svoboda <rasvob14@gmail.com>
2017-08-03 22:48:55 +0200

    View modified

--- PotPlayerAPI/Program.cs
+++ PotPlayerAPI/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 
 namespace PotPlayerAPI
 {
@@ -13,7 +15,7 @@ namespace PotPlayerAPI
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
-                //.UseUrls("http://192.168.2.7:56111")
+                .UseUrls("http://localhost:56111;http://192.168.2.7:56111")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
@@ -21,6 +23,38 @@ namespace PotPlayerAPI
                 .Build();
 
             host.Run();
+
+            //bool isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            //foreach (string s in args)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //var pathToContentRoot = Directory.GetCurrentDirectory();
+            //if (isService)
+            //{
+            //    var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            //    pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            //}
+
+            //var host = new WebHostBuilder()
+            //    .UseKestrel()
+            //    .UseUrls("http://localhost:56111;http://192.168.2.7:56111")
+            //    .UseContentRoot(pathToContentRoot)
+            //    .UseIISIntegration()
+            //    .UseStartup<Startup>()
+            //    .UseApplicationInsights()
+            //    .Build();
+
+            //if (isService)
+            //{
+            //    host.RunAsService();
+            //}
+            //else
+            //{
+            //    host.Run();
+            //}
         }
     }
 }
